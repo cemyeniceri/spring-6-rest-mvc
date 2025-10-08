57980a5dec97c6c35685ca2f57e9acb00cbb6745
cyeniceri <cem.yeniceri@gmail.com>
2025-10-09 00:37:27 +0100

    open api validation with restassured

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -2,18 +2,23 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
+
         return http.build();
     }
 }
