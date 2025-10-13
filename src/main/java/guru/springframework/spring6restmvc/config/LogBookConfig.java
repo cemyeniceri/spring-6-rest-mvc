6f48abcb254fee16e4d64d564f11d3e18a05b395
cyeniceri <cem.yeniceri@gmail.com>
2025-10-13 01:45:19 +0100

    logbook added

---
+++ src/main/java/guru/springframework/spring6restmvc/config/LogBookConfig.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.HttpLogFormatter;
+import org.zalando.logbook.Sink;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.logstash.LogstashLogbackSink;
+
+@Configuration
+public class LogBookConfig {
+    @Bean
+    public Sink logBookLogStash() {
+        HttpLogFormatter jsonHttpLogFormatter = new JsonHttpLogFormatter();
+        LogstashLogbackSink logstashLogbackSink = new LogstashLogbackSink(jsonHttpLogFormatter);
+        return logstashLogbackSink;
+    }
+}
